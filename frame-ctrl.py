6c2a67a02423a018a7e3557752dbf15f91359dbb
Daniel Hall <daniel@danielhall.me>
2012-05-18 19:19:42 +1000

    more refactoring

--- frame-ctrl.py
+++ frame-ctrl.py
@@ -8,7 +8,8 @@ import usb.core
 from usb.util import *
 
 vendorId = 0x04e8
-models = {'SPF-87H': (0x2033, 0x2034), 'SPF-107H': (0x2027, 0x2028) }
+prodIdStore = 0x2033
+prodIdDisp  = 0x2034
 
 chunkSize = 0x4000
 bufferSize = 0x20000
@@ -44,26 +45,19 @@ def writeImage(dev):
 
 found = False
 
-for k, v in models.iteritems():
-  dev = usb.core.find(idVendor=vendorId, idProduct=v[0])
-  if dev:
-    print "Found " + k + " in storage mode"
-    try:
-      dev.ctrl_transfer(CTRL_TYPE_STANDARD | CTRL_IN | CTRL_RECIPIENT_DEVICE, 0x06, 0xfe, 0xfe, 254)
-    except usb.core.USBError as e:
-      errorStr = str(e)
-    time.sleep(1)
-    dev = usb.core.find(idVendor=vendorId, idProduct=v[1])
-    found = True
-  if not dev:
-    dev = usb.core.find(idVendor=vendorId, idProduct=v[1])
-  if dev:
-    print "Found " + k + " in display mode"
-    dev.set_configuration()
-    result = dev.ctrl_transfer(CTRL_TYPE_VENDOR | CTRL_IN | CTRL_RECIPIENT_DEVICE, 0x04, 0x00, 0x00, 1)
-    writeImage(dev)
-    found = True
+dev = usb.core.find(idVendor=vendorId, idProduct=prodIdStore)
+if dev:
+  try:
+    dev.ctrl_transfer(CTRL_TYPE_STANDARD | CTRL_IN | CTRL_RECIPIENT_DEVICE, 0x06, 0xfe, 0xfe, 254)
+  except usb.core.USBError as e:
+    errorStr = str(e)
+  time.sleep(1)
 
-if not found:
-  print >> sys.stderr, "No supported devices found"
-  sys.exit(-1)
+dev = usb.core.find(idVendor=vendorId, idProduct=prodIdDisp)
+if dev:
+  dev.set_configuration()
+  result = dev.ctrl_transfer(CTRL_TYPE_VENDOR | CTRL_IN | CTRL_RECIPIENT_DEVICE, 0x04, 0x00, 0x00, 1)
+  writeImage(dev)
+elif not dev:
+  print "Device not found"
+
